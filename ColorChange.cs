7605b9c79fd92702ace1f61ade4e854aedde00af
Yuto-x <57879864+Yuto-x@users.noreply.github.com>
2020-02-07 09:33:31 +0900

    Add files via upload

---
+++ ColorChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    private RaycastHit hit;
+    string CreateObjName; // 生成したオブジェクトの名前
+    GameObject CreateObjTag; // 生成したオブジェクトのタグ
+    Material mat;
+    Material materi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        materi = Resources.Load<Material>("Materials/orenji");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                CreateObjName = hit.collider.gameObject.name;
+                CreateObjTag = GameObject.Find(CreateObjName);
+                mat = CreateObjTag.GetComponent<Renderer>().material;
+                CreateObjTag.GetComponent<Renderer>().material = materi;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                CreateObjTag.GetComponent<Renderer>().material = mat;
+
+            }
+        }
+    }
+}
